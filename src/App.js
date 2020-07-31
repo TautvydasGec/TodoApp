5b0041b9278e6cdadaa7ddf2356a7a49619eb350
TautvydasGec <gecevicius.tautvydas@gmail.com>
2020-07-31 14:55:07 +0300

    Added input and display of todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList';
+import uuid from 'uuid/dist/v4';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const taskNameRef = useRef()
+
+  function handleAdd(e) {
+    const name = taskNameRef.current.value
+    if (name ==="") return
+    setTasks(prevTasks => {
+      return [{ id: uuid(), name: name, complete: false},...prevTasks ]
+    })
+    taskNameRef.current.value = null
+  }
+
   return ( 
     <div className = "App" >
-    Hello world 
+      <input ref={taskNameRef} type="text" />
+      <button onClick={handleAdd}>Add</button>
+      <TodoList tasks={tasks}/>
     </div>
   );
 }
