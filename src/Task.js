16f35e9b3d582e2e180c7e0056ff6be62b7b3d90
TautvydasGec <gecevicius.tautvydas@gmail.com>
2020-08-03 12:36:24 +0300

    Added emoji role and aria-label

--- src/Task.js
+++ src/Task.js
@@ -18,7 +18,7 @@ export default function Task({ task, toggleComplete, handleDelete }) {
           checked={task.complete} 
           onChange={handleCheckClick}
         />
-        {task.name} {task.date} {task.late? <span>&#128308;</span> : ""} {/*if a task is late a red indicator is shown */}
+        {task.name} {task.date} {task.late? <span role="img" aria-label="red dot">&#128308;</span> : ""} {/*if a task is late a red indicator is shown */}
       </label>
       <button onClick={handleCheckDelete}>Delete</button>
     </div>
